712f1f987176191d3ec682de90e606c509fd30b5
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-05-04 10:54:19 +0000

    task 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update && apt-get install -y git curl
-ded_0.84.0_Linux-64bit.deb -o hugo-v0.84.deb
+curl https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo-v0.84.deb
 apt install ./hugo-v0.84.deb
 make build
